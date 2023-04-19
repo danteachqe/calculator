a8419a73d87598dadffc210b3edd582c4efecf7d
danteachqe <dan.teachqe@gmail.com>
2023-04-19 13:40:48 +0300

    new port file configuration

--- src/main/java/com/CalculatorApplication.java
+++ src/main/java/com/CalculatorApplication.java
@@ -1,11 +1,15 @@
 package com;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+        SpringApplication app = new SpringApplication(CalculatorApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
+        app.run(args);
     }
 }
