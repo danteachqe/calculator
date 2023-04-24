ef445244ef3f7d488773f18071dfbb41505e4a3d
danteachqe <dan.teachqe@gmail.com>
2023-04-24 14:34:04 +0300

    Added security library to POM

--- src/main/java/com/CalculatorApplication.java
+++ src/main/java/com/CalculatorApplication.java
@@ -3,6 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Collections;
+import org.owasp.encoder.Encode;
 
 @SpringBootApplication
 public class CalculatorApplication {
@@ -11,5 +12,9 @@ public class CalculatorApplication {
         SpringApplication app = new SpringApplication(CalculatorApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
         app.run(args);  
+    // Example of using org.owasp.encoder to encode user input
+    String userInput = "<script>alert('XSS attack!');</script>";
+    String encodedInput = Encode.forHtml(userInput);
+    System.out.println(encodedInput);    
     }
 }
